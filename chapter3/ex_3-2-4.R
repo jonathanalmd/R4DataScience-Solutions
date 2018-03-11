e397fed5f16be3b8519f052d7133530eaeccccab
Jonathan Mendes de Almeida <jonathanalmd@gmail.com>
2018-03-11 00:11:46 -0300

    Add R file for solutions to 3.2.4 exercises

---
+++ chapter3/ex_3-2-4.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+mpg = ggplot2::mpg
+mpg
+
+# Exercise 1
+ggplot(data = mpg)
+
+# Exercise 2
+nrows = nrow(mpg)
+ncols = ncol(mpg)
+print(nrows)
+print(ncols)
+
+# Just to check
+glimpse(mpg)
+
+
+# Exercise 3
+mpg['drv']
+# There are 3 possible values for __drv__ variable (**variable** : _description_):
+
+
+# Exercise 4
+ggplot(data = mpg) + geom_point(mapping = aes(x = hwy, y = cyl, colour = class))
+# We add a definition of the data used in x,y axis and add geom_point function mapping these points. 
+# Here we are using aes(colour = class) (to associate the name of the aesthetic with a variable to display) 
+# to plot using a different colour for each class present in our data frame 
+# (car type: 2seater, compact, midsize, minivan, pickup, subcompact, suv).
+
+# Exercise 5
+ggplot(data = mpg, aes(x = class, y = drv)) + geom_point()
+# This plot is not useful because **class** and **drv** are factor variables. 
+# Each possible value of these two variables is limited by a set 
+# set (r, f and 4 are the possible values for **drv**  and 2seater, compact, midsize, minivan, pickup, subcompact and suv are the possible values for **class**).
+# This plot is pretty useless to perform a data analysis.
+
+
+
+
+
